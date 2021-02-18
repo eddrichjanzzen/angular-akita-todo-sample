455b9de3a203d3d1f914cca0c64b7bda5087b1fa
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-18 22:15:34 +0800

    add routing module for authentication

---
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -0,0 +1,21 @@
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthenticationRoutingModule { }
--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AuthenticationComponent } from './authentication.component';
-
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent, SignUpComponent, AuthenticationComponent],
+  declarations: [
+    SignUpComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthenticationRoutingModule
   ]
 })
 export class AuthenticationModule { }
