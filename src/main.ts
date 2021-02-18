1692bfa1610373c0d6a774b9312191bc18f057e6
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-18 21:55:10 +0800

    add persists storage to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { persistState } from '@datorama/akita';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const storage = persistState();
+
+const providers = [{ provide: 'persistStorage', useValue: storage }];
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
