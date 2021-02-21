e1b4509394b1eabe65a569dfded2d8e8461a4ff9
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-22 00:12:59 +0800

    import http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
+import { MainModule } from './modules/main/main.module';
 import { SharedModule } from './modules/shared/shared.module';
-import { CustomMaterialModule } from './modules/custom-material/custom-material.module';
 import { CoreModule } from './core/core.module';
-import { RouterModule } from '@angular/router';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,27 +11,28 @@ import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainComponent } from './modules/main/main.component';
-import { TodoComponent } from './modules/todo/todo.component';
+import { ApiHttpService } from './core/services/api-http.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    TodoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
-    SharedModule,
     CoreModule,
     AuthenticationModule,
+    MainModule,
+    SharedModule,
     AppRoutingModule,
+    HttpClientModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
     AkitaNgRouterStoreModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ApiHttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
