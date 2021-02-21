759b44dbcade7de543b51101ba6246fef71fb585
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-21 20:12:11 +0800

    initial design for login component

--- src/app/modules/authentication/login/login.component.ts
+++ src/app/modules/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  hide: boolean;
+
+  constructor() {
+    this.hide = true;
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onLogin(): void {
+    if (!this.loginForm.valid){
+      return;
+    }
   }
 
 }
