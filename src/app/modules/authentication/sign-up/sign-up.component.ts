8e0ef1c4de383374dd965717fdf6018519e8444f
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-21 20:12:23 +0800

    initial design for sign up component

--- src/app/modules/authentication/sign-up/sign-up.component.ts
+++ src/app/modules/authentication/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.signUpForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name : new FormControl('', [Validators.required, Validators.minLength(6)]),
+      age: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), this.checkIfMatchingPasswords]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), this.checkIfMatchingPasswords])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSignUp(): void {
+    if (!this.signUpForm.valid){
+      return;
+    }
+  }
+
+  private checkIfMatchingPasswords(formGroup: FormGroup): ValidationErrors | null {
+    const password = formGroup.get('password');
+    const confirmPassword = formGroup.get('confirmPassword');
+
+    return password?.value !== '' && confirmPassword?.value !== ''
+      && password?.value !== confirmPassword?.value ? { matching: true } : null;
   }
 
 }
