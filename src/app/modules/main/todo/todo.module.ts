413618dbaff3268026dfef1cd9587b3d7e8f2db2
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-22 00:15:10 +0800

    declare todo and todo-list components in todo module

--- src/app/modules/main/todo/todo.module.ts
+++ src/app/modules/main/todo/todo.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoComponent } from './todo.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 
 @NgModule({
-  declarations: [TodoComponent],
+  declarations: [
+    TodoComponent,
+    TodoListComponent
+  ],
   imports: [
     CommonModule,
     TodoRoutingModule
