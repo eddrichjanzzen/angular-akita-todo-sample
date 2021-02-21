a31e5ed6c1c01f90eaa2283de25ea4fc7babf50e
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-22 00:14:06 +0800

    add navigation for loginUser

--- src/app/modules/authentication/state/session.service.ts
+++ src/app/modules/authentication/state/session.service.ts
@@ -1,15 +1,17 @@
-import { RegisterUserRequestModel, RegisterUserResponseModel } from './../../../core/models/user.model';
+import { RegisterUserRequestModel, RegisterUserResponseModel, LoginUserRequestModel, LoginUserResponseModel } from './../../../core/models/user.model';
 import { UserDataService } from './../../../core/services/user-data.service';
 import { Inject, Injectable } from '@angular/core';
 
 import { SessionStore } from './session.store';
 import { PersistState } from '@datorama/akita';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class SessionService {
 
   constructor(private sessionStore: SessionStore,
               private userDataService: UserDataService,
+              private router: Router,
               @Inject('persistStorage') private persistStorage: PersistState,
               ) {
   }
@@ -23,13 +25,33 @@ export class SessionService {
     .subscribe((userData: RegisterUserResponseModel) => {
       if (userData.token){
         // update the session store here
-        this.sessionStore.update(state => {
-          return {
-            ...state.UserData,
-            UserData: userData
-          };
-        });
+        this.sessionStore.register(userData);
         this.sessionStore.setLoading(false);
+      }
+
+    }, (err) => {
+      // set the error state
+      this.sessionStore.setError(err);
+      this.sessionStore.setLoading(false);
+
+    });
+
+  }
+
+
+  loginUser(request: LoginUserRequestModel): void {
+    this.persistStorage.clearStore();
+
+    this.sessionStore.setLoading(true);
+
+    this.userDataService.loginUser(request)
+    .subscribe((userData: LoginUserResponseModel) => {
+      if (userData.token !== null){
+
+        // update the session store here
+        this.sessionStore.login(userData);
+        this.sessionStore.setLoading(false);
+        this.router.navigateByUrl('/todos');
       }
 
     }, (err) => {
