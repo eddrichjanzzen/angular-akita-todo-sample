362d28262db811ea5059ae36582a4cabcd026d09
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-21 11:04:26 +0800

    import core module and custom material module to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { CustomMaterialModule } from './modules/custom-material/custom-material.module';
+import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,18 +10,24 @@ import { AppComponent } from './app.component';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './modules/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule,
+    CustomMaterialModule,
+    CoreModule,
     AuthenticationModule,
     AppRoutingModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
-    AkitaNgRouterStoreModule
+    AkitaNgRouterStoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
