9d838e134e89e97edb6e9437dcf60104cef5758b
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-28 22:33:34 +0800

    important: add definition for authentication component in authentication module

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -4,9 +4,11 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthenticationComponent } from './authentication.component';
 
 @NgModule({
   declarations: [
+    AuthenticationComponent,
     SignUpComponent,
     LoginComponent
   ],
