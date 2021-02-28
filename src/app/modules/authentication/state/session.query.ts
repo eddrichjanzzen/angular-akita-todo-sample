43ccf0d9a0de5de895524bbb83234f8527e1fbe4
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-03-01 00:07:40 +0800

    add isLoggedIn() function

--- src/app/modules/authentication/state/session.query.ts
+++ src/app/modules/authentication/state/session.query.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Query } from '@datorama/akita';
+import { Query, toBoolean } from '@datorama/akita';
 import { SessionStore, SessionState } from './session.store';
+import { take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class SessionQuery extends Query<SessionState> {
+
+  checkToken$ = this.select((state) => toBoolean(state.UserSession?.tokens));
+  displayName$ = this.select((state) => state.UserSession.display_name);
+
+  isLoggedIn$ = this.checkToken$.pipe(take(1));
 
   constructor(protected store: SessionStore) {
     super(store);
   }
 
+  isLoggedIn(): boolean {
+    let isLoggedIn = false;
+
+    this.isLoggedIn$.pipe(
+      take(1)
+    ).subscribe((result: boolean) => {
+      isLoggedIn = result;
+    });
+
+    return isLoggedIn;
+
+  }
 }
+
+
