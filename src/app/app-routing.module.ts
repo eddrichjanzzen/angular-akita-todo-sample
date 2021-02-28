4b505f9a277cf9be38bf690b494c6af021b81fa4
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-28 22:33:08 +0800

    lazy load main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
   }
 ];
 
