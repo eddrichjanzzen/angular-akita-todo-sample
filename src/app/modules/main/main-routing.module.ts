9258e45f20c2448fb019758d6e3a45c9291f54da
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-28 22:32:39 +0800

    lazyload authentication module and todo modules

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,13 +1,20 @@
+import { AuthenticationComponent } from './../authentication/authentication.component';
 import { MainComponent } from './main.component';
 import { TodoComponent } from './todo/todo.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
     children: [
+      {
+        path: '',
+        component: AuthenticationComponent,
+        loadChildren: () => import('./../authentication/authentication.module').then(m => m.AuthenticationModule)
+      },
       {
         path: 'todos',
         component: TodoComponent,
