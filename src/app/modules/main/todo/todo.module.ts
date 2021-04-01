a18d4aa97c38d6f1fb44d356020c29e72b97b2aa
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-04-01 20:51:48 +0800

    Add shared module

--- src/app/modules/main/todo/todo.module.ts
+++ src/app/modules/main/todo/todo.module.ts
@@ -1,18 +1,22 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoComponent } from './todo.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 
 @NgModule({
   declarations: [
     TodoComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     CommonModule,
+    SharedModule,
     TodoRoutingModule
   ]
 })
