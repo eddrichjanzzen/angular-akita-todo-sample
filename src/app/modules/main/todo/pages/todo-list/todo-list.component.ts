7227cffa36e5469ae2e5ea92c723c6e67f4bb7f2
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-04-01 20:53:40 +0800

    Add initial logic for todo list component, displays all todos

--- src/app/modules/main/todo/pages/todo-list/todo-list.component.ts
+++ src/app/modules/main/todo/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../state/todo.service';
+import { TodoModel } from 'src/app/core/models/todo.model';
+import { TodoQuery } from '../../state/todo.query';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  todos$: Observable<Array<TodoModel>>;
+  isLoading$: Observable<boolean>;
+
+  constructor(private todoService: TodoService,
+              private todoQuery: TodoQuery) { }
 
   ngOnInit(): void {
+    this.todoService.fetchAllTodos();
+
+    this.todos$ = this.todoQuery.selectAll();
+    this.isLoading$ = this.todoQuery.selectLoading();
   }
 
 }
