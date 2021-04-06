452a3f1e6086baecc871be0a143804f3207ab5ba
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-04-06 20:29:30 +0800

    add click event for updating todo

--- src/app/modules/main/todo/components/todo-item/todo-item.component.ts
+++ src/app/modules/main/todo/components/todo-item/todo-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TodoModel } from 'src/app/core/models/todo.model';
+import { TodoQuery } from '../../state/todo.query';
+import { TodoService } from '../../state/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,9 +11,16 @@ import { TodoModel } from 'src/app/core/models/todo.model';
 export class TodoItemComponent implements OnInit {
   @Input() todoItem: TodoModel;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
+
+  toggle(todoItem: TodoModel): void {
+    this.todoService.updateTodo(todoItem.id, todoItem);
+    console.log("toggled")
+    // alert("toggled");
+  }
+
 
 }
