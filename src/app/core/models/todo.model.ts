3a6bfae2039e10050c3fb723d97550488eed02cb
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-04-06 20:29:06 +0800

    add update todo models

--- src/app/core/models/todo.model.ts
+++ src/app/core/models/todo.model.ts
@@ -1,13 +1,16 @@
-import { PaginatedResponseModel } from './generic.model';
+import { PaginatedResponseModel, ResponseModel } from './generic.model';
+
 export interface TodoModel {
   id: string;
   title: string;
   description: string;
-  created_date: Date;
-  updated_date: Date;
+  created_date?: Date;
+  updated_date?: Date;
   completed: boolean;
 }
 
-export interface GetAllTodosResponseModel extends PaginatedResponseModel<TodoModel>{
+export interface GetAllTodosResponseModel extends PaginatedResponseModel<TodoModel>{ }
 
-}
+export interface UpdateTodoRequestModel extends TodoModel { }
+
+export interface UpdateTodoResponseModel extends ResponseModel<TodoModel> { }
