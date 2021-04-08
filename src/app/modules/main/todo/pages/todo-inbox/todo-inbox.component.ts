3d04327ee381641e61932be16967fe24dfefc0af
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-04-08 23:12:54 +0800

    Implement todo inbox

---
+++ src/app/modules/main/todo/pages/todo-inbox/todo-inbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-inbox',
+  templateUrl: './todo-inbox.component.html',
+  styleUrls: ['./todo-inbox.component.scss']
+})
+export class TodoInboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
