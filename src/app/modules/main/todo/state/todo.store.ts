4eeddfce1594076164c7672a92b34a9181598892
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-05-08 12:07:28 +0800

    Clean up todo store

--- src/app/modules/main/todo/state/todo.store.ts
+++ src/app/modules/main/todo/state/todo.store.ts
@@ -1,30 +1,17 @@
 import { Injectable } from '@angular/core';
-import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+import { ActiveState, EntityState, EntityStore, StoreConfig } from '@datorama/akita';
 import { TodoModel } from 'src/app/core/models/todo.model';
 
+// remember to inclue active state
+export interface TodoState extends EntityState<TodoModel, string>, ActiveState {}
 
-export interface TodoState extends EntityState<TodoModel> {
-  ui: {
-    isLoading: boolean;  
-  };
-}
-
-const initialState = {
-  ui: { 
-    isLoading: false
-  }
-};
 
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'todo' })
 export class TodoStore extends EntityStore<TodoState, TodoModel> {
 
   constructor() {
-    super(initialState);
-  }
-
-  todoULoading(isLoading: boolean){
-    this.update({ui: {isLoading}})
+    super();
   }
 
 }
