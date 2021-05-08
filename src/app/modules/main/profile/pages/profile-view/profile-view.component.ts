63884845c638717679738afb7918ff7d91d4a957
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-05-09 00:42:14 +0800

    Retrieve from profile query

--- src/app/modules/main/profile/pages/profile-view/profile-view.component.ts
+++ src/app/modules/main/profile/pages/profile-view/profile-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { UserModel } from 'src/app/core/models/user.model';
 import { ProfileQuery } from '../../state/profile.query';
 import { ProfileService } from '../../state/profile.service';
@@ -17,7 +18,11 @@ export class ProfileViewComponent implements OnInit {
     private profileService: ProfileService) { }
 
   ngOnInit(): void {
-
+    this.profileQuery.selectActive().pipe(
+      take(1))
+      .subscribe((profile) => {
+      this.profileInfo = profile;
+    })
   }
 
 }
