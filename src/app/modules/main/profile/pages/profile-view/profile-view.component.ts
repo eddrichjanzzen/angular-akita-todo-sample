47d1d74a7b94d60197d221b81170ec6599c2a607
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-05-08 21:30:39 +0800

    Update profile query

--- src/app/modules/main/profile/pages/profile-view/profile-view.component.ts
+++ src/app/modules/main/profile/pages/profile-view/profile-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/core/models/user.model';
+import { ProfileQuery } from '../../state/profile.query';
+import { ProfileService } from '../../state/profile.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+  profileInfo?: UserModel;  
+
+  constructor(
+    private profileQuery: ProfileQuery,
+    private profileService: ProfileService) { }
 
   ngOnInit(): void {
+
   }
 
 }
