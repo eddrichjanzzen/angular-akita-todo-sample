7da54490bb1b5769c3262f6976fabbca54c238ca
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-05-08 21:30:51 +0800

    Add components profile module

--- src/app/modules/main/profile/profile.module.ts
+++ src/app/modules/main/profile/profile.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from '../../shared/shared.module';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileViewComponent } from './pages/profile-view/profile-view.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    ProfileViewComponent,
+    ProfileCardComponent
   ],
   imports: [
     CommonModule,
