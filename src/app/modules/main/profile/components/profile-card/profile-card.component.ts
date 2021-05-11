481d20c31456c39bbac4b4040c50587e4f975245
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-05-11 21:19:12 +0800

    Implement update profile functionality

--- src/app/modules/main/profile/components/profile-card/profile-card.component.ts
+++ src/app/modules/main/profile/components/profile-card/profile-card.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { skip } from 'rxjs/operators';
 import { UserModel } from 'src/app/core/models/user.model';
 import { ProfileQuery } from '../../state/profile.query';
+import { ProfileService } from '../../state/profile.service';
 
 @Component({
   selector: 'app-profile-card',
@@ -11,24 +14,33 @@ import { ProfileQuery } from '../../state/profile.query';
 export class ProfileCardComponent implements OnInit {
 
   profileForm: FormGroup;
+  isLoading$ : Observable<boolean>;
   @Input() profileInfo?: UserModel;
 
-  constructor(private profileQuery: ProfileQuery) { 
+  constructor(
+    private profileQuery: ProfileQuery,
+    private profileService: ProfileService) { 
     this.profileForm = new FormGroup({
-      email: new FormControl('', Validators.minLength(6)),
-      display_name: new FormControl('', Validators.minLength(6)),
-      date_created: new FormControl(''),
+      email: new FormControl({ value: '', disabled: true}),
+      display_name: new FormControl('', Validators.minLength(6))
     });
   }
 
   ngOnInit(): void {
+    this.profileForm.setValue({
+      email: this.profileInfo?.email,
+      display_name: this.profileInfo?.display_name
+    });
 
-      this.profileForm.setValue({
-        email: this.profileInfo?.email,
-        display_name: this.profileInfo?.display_name,
-        date_created: this.profileInfo?.created_date
-      })
+    this.isLoading$ = this.profileQuery.selectLoading().pipe(skip(1));
+  }
 
+  updateProfile(): void {
+    if(this.profileForm.valid){
+      this.profileService.updateProfile({
+        display_name: this.profileForm?.get('display_name')?.value
+      });
+    }
   }
 
 }
